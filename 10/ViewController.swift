615ff24b8abf8818210dd076ff1e70f95ce1db96
nakanodesukun <94818124+nakanodesukun@users.noreply.github.com>
2022-01-17 16:46:27 +0900

    修正しました

--- 10/ViewController.swift
+++ 10/ViewController.swift
@@ -8,32 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
-    let backgroundColor = ["CellRed", "CellGreen", "CellBlue"]
-    let prefectures = ["北海道", "青森県", "岩手県", "宮城県", "秋田県","山形県," ,"福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
 
-
+    private let model = Model()
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return prefectures.count
+        return model.prefectures.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var identifier = ""
 
-        switch indexPath.row % 3{
-        case 0: identifier = backgroundColor[0]
-        case 1: identifier = backgroundColor[1]
-        case 2: identifier = backgroundColor[2]
+        switch indexPath.row % 3 {
+        case 0: identifier = model.backgroundColor[0]
+        case 1: identifier = model.backgroundColor[1]
+        case 2: identifier = model.backgroundColor[2]
         default:
             break
         }
         let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
-        cell.textLabel?.text = prefectures[indexPath.row]
+        cell.textLabel?.text = model.prefectures[indexPath.row]
         cell.detailTextLabel?.text = "\(indexPath.row + 1)番目の都道府県です"
         return cell
     }
-
-
-
 }
-
