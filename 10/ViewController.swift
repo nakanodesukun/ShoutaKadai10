2b3af86d303b25a7a0fa1726fe2d95cb06266736
nakanodesukun <94818124+nakanodesukun@users.noreply.github.com>
2022-01-20 16:13:21 +0900

    akioさんのフィードバックをもとに修正しました!

--- 10/ViewController.swift
+++ 10/ViewController.swift
@@ -16,16 +16,11 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var identifier = ""
+        // これはViewに関する知識なので Model には持たせないようが良さそうです
+        let identifiers = ["CellRed", "CellGreen", "CellBlue"]
+        // identifier 決定の処理をシンプルにしました
+        let identifier = identifiers[indexPath.row % identifiers.count]
 
-        switch indexPath.row % 3 {
-        case 0: identifier = model.backgroundColor[0]
-        case 1: identifier = model.backgroundColor[1]
-        case 2: identifier = model.backgroundColor[2]
-        default:
-            break
-        }
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
         cell.textLabel?.text = model.prefectures[indexPath.row]
         cell.detailTextLabel?.text = "\(indexPath.row + 1)番目の都道府県です"
